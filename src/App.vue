646ca7e9c905fec96322ab5c86860c8c4f05323e
yanqiansun <yanqiansun@tencent.com>
2021-12-06 12:13:18 +0800

    feat: 修改部分信息 --story=000000

--- src/App.vue
+++ src/App.vue
@@ -24,9 +24,8 @@
         fullStyle: [
           `/*
 * Inspired by http://strml.net/
-* 大家好，我是方方
-* 二月了，好多公司都在招聘，你是不是也在准备简历呀。
-* 说做就做，我也来写一份简历！
+* 你好，我叫孙燕倩
+* 这是我准备的一份动态小简历
 */
 
 /* 首先给所有元素加上过渡效果 */
@@ -115,10 +114,15 @@ html{
 }
 `],
         currentMarkdown: '',
-        fullMarkdown: `方应杭
+        fullMarkdown: `孙燕倩
 ----
 
-资深前端工程师，资深前端讲师，现在在 [饥人谷](http://jirengu.com) 教前端课程。
+意向职位：前端工程师
+
+学历
+----
+华南师范大学     软件工程  本科
+中国科学技术大学 电子信息  硕士
 
 技能
 ----
@@ -126,23 +130,24 @@ html{
 * 前端开发
 * Rails 开发
 * Node.js 开发
-* 前端授课
+* vue 开发
 
-工作经历
+实习经历
 ----
+腾讯视频搜索技术中心 2021/8 - 2021/3
 
-1. [饥人谷](http://jirengu.com)
-2. 腾讯即时通讯平台部
-3. 阿里巴巴B2B部门
-4. 彩程知人项目组
+项目经历
+----
+http多路由切换 已全量上线
+参与腾讯视频9.0改版
 
 链接
 ----
 
-* [GitHub](https://github.com/frankfang)
-* [我的文章](https://www.zhihu.com/people/zhihusucks/pins/posts)
+* [GitHub](https://github.com/aLittleSevenCat)
+* [我的博客](https://blog.csdn.net/weixin_41950078)
 
-> 如果你喜欢这个效果，Fork [我的项目](https://github.com/jirengu-inc/animating-resume)，打造你自己的简历！
+> 如果您觉得合适，请给我面试机会，感谢！
 
 `
       }
